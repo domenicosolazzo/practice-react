5be47c98df8805f91debb98f05b2b5490aac2061
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-02-21 18:55:03 +0100

    Added the ProductTable

--- thinkingreact/scripts/app.js
+++ thinkingreact/scripts/app.js
@@ -19,3 +19,30 @@ var ProductRow = React.createClass({
 		);
 	}
 });
+
+var ProductTable = React.createClass({
+	render: function(){
+		var rows = [];
+		var lastCategory = null;
+		this.props.products.forEach(function(product){
+			if (product.category !== lastCategory){
+				rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+			}
+			rows.push(<ProductRow product={product} key={product.name} />);
+			lastCategory = product.category;
+
+		});
+
+		return (
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>{rows}</tbody>
+			</table>
+		);
+	}
+});
