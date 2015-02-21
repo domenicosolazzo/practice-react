50c0b56f03c71b36b37775148d478ae640748f0e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-02-21 13:00:14 +0100

    Adding an external comments.json and improving the CommentBox

--- official/server.js
+++ official/server.js
@@ -8,23 +8,24 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/comments.json', function(req, res){
-	fs.readFile('_comment.json', function(err, data){
-		res.setHeader('Content-Type', 'application/json');
-		res.send(data);
-	});
+app.get('/comments.json', function(req, res) {
+  fs.readFile('_comments.json', function(err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
 });
 
-app.post('/comments.json', function(req, res){
-	fs.readFile('_comments.json', function(err, data){
-		var comments = JSON.parse(data);
-		comments.push(req.body);
-		fs.writeFile('_comments.json', JSON.stringify(comments, null, 4), function(err){
-			res.setHeader('Content-Type', 'application/json');
-			res.send(JSON.stringify(comments));
-		});
-	});
+app.post('/comments.json', function(req, res) {
+  fs.readFile('_comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('_comments.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
+    });
+  });
 });
+
 
 app.listen(3000);
 
