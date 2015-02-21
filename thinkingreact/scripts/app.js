a43c599b77824e9246eb9a92dbcaea53b4fb4548
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-02-21 18:57:44 +0100

    Added the SearchBar

--- thinkingreact/scripts/app.js
+++ thinkingreact/scripts/app.js
@@ -39,10 +39,26 @@ var ProductTable = React.createClass({
 					<tr>
 						<th>Name</th>
 						<th>Price</th>
+
 					</tr>
 				</thead>
 				<tbody>{rows}</tbody>
 			</table>
+		);
+	}
+});
+
+var SearchBar = React.createClass({
+	render: function(){
+		return (
+			<form>
+				<input type="text" placeholder="Search..." />
+				<p>
+					<input type="checkbox" />
+					{' '}
+					Only show products in stock
+				</p>
+			</form>
 		);
 	}
 });
