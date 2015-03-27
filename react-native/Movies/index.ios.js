636bba3b127aab77addc5eeb40a61326d27172f9
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-28 00:54:38 +0100

    Adding api information for retrieving the movies

--- react-native/Movies/index.ios.js
+++ react-native/Movies/index.ios.js
@@ -8,6 +8,12 @@ var React = require('react-native');
 var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
 var {
   AppRegistry,
   Image,
