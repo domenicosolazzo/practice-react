f9878618a53d3699a0f44dec6249b072370603df
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-27 21:48:18 +0100

    Adding mock data

--- react-native/Movies/index.ios.js
+++ react-native/Movies/index.ios.js
@@ -5,6 +5,9 @@
 'use strict';
 
 var React = require('react-native');
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 var {
   AppRegistry,
   StyleSheet,
